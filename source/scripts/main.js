a57da12fbe2711af0a583a97d0554201084902cb
Anna <annabaidikova92@gmail.com>
2025-01-22 00:08:44 +0100

    gallery-swiper, block services added

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,3 +1,9 @@
+// import Swiper from 'swiper/bundle';
+// import { Navigation } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+
 import { initializeGallerySwiper } from './swiper-gallery';
 
 initializeGallerySwiper();
--- source/scripts/swiper-gallery.js
+++ source/scripts/swiper-gallery.js
@@ -1,39 +1,41 @@
 import Swiper from 'swiper/bundle';
 import { Navigation } from 'swiper/modules';
-// import Swiper styles
 import 'swiper/css/bundle';
 import 'swiper/css/navigation';
 
 
 export function initializeGallerySwiper() {
-  const reviewsSwiper = new Swiper('.gallery__swiper', {
+  const gallerySwiper = new Swiper('.gallery__swiper', {
     modules: [Navigation],
-    loop: false,
-    // grabCursor: true,
+    loop: true,
+    grabCursor: true,
     normalizeSliderIndex: true,
     direction: 'horizontal',
     navigation: {
       nextEl: '.gallery__button.swiper-button-next',
       prevEl: '.gallery__button.swiper-button-prev',
     },
-    slidesPerGroup: 1,
-    slidesPerView: 'auto',
-    // breakpoints: {
-    //   320: {
-    //     slidesPerView: 1,
-    //     spaceBetween: 15,
-    //     centeredSlides: true,
-    //   },
-    //   768: {
-    //     slidesPerView: 3,
-    //     spaceBetween: 30,
-    //     // slidesOffsetAfter: 30,
-    //   },
-    //   1440: {
-    //     slidesPerView: 2,
-    //     // spaceBetween: 120,
-    //   },
-    // },
+    // slidesPerView: 1, // Отображать 1 слайд на экране
+    // spaceBetween: 20,
+    // slidesPerGroup: 1,
+    // slidesPerView: 'auto',
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        centeredSlides: true,
+        initialSlide: 9,
+
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        // slidesOffsetAfter: 30,
+      },
+      1440: {
+        slidesPerView: 2,
+        // spaceBetween: 120,
+      },
+    },
   });
 
   const prevButton = document.querySelector('.gallery__button.swiper-button-prev');
@@ -45,13 +47,13 @@ export function initializeGallerySwiper() {
 
     prevButton.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
-        reviewsSwiper.slidePrev();
+        gallerySwiper.slidePrev();
       }
     });
 
     nextButton.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
-        reviewsSwiper.slideNext();
+        gallerySwiper.slideNext();
       }
     });
   }
